dd6a91bf689039890f39a6b3ebd8c4fc571122fc
inauner <inaun@pm.me>
2020-02-23 22:14:40 -0800

    Update tcode_generator.py
    fix floats

--- tcode_generator.py
+++ tcode_generator.py
@@ -2,26 +2,75 @@ import serial #pip install pySerial
 import time
 import random
 import sys
+import platform
+
 
 #arduino COM port
 COM = 'COM4'
 
+# A function that tries to list serial ports on most common platforms
+def list_serial_ports():
+    system_name = platform.system()
+    if system_name == "Windows":
+        # Scan for available ports.
+        available = []
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                available.append(i)
+                s.close()
+            except serial.SerialException:
+                pass
+        return available
+    elif system_name == "Darwin":
+        # Mac
+        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
+    else:
+        # Assume Linux or something else
+        return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
+        
+
+def OSRneutral():
+    with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
+        i = b'L05R15\n'
+        ser.write(i)
+
+def OSRgrind():
+    # with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
+    for n in range(5):
+        i = b'R1999I500\n'
+        time.sleep(.5)
+        ser.write(i)
+        i = b'R1000I500\n'
+        time.sleep(.5)
+        ser.write(i)
+        return(i)
+
 def OSR2():
-    last_vertDist=1
+    min_range=100
+    max_range=699
+    min_time=200
+    max_time=700
+    last_vertDist=0
     last_grindDist=500
     with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
         while True:
-            vertDist = random.randint(100,999)/1000 #need to make it a float for min. vert stroke distance
-            vertDur = random.randint(400,1200) #length of time in ms for a stroke to complete
+            vertDist = float(random.randint(min_range,max_range))/1000 #need to make it a float for min. vert stroke distance
+            vertDur = random.randint(min_time,max_time) #length of time in ms for a stroke to complete
             grindDur = random.randint(1,999)
             #make sure vertical stroke distance is at least 250 between commands
-            while abs(vertDist-last_vertDist/1000)<.7:
-                vertDist = random.randint(100,999)/1000
+            
+            while abs(vertDist-last_vertDist)<.4:
+                vertDist = float(random.randint(min_range,max_range))/1000
+                # print(vertDist,last_vertDist,abs(vertDist-last_vertDist))
+                
             vertDist = vertDist * 1000
             #randomize a grinding motion
             ranRotate = random.randint(3,3)
             if ranRotate == 1:
-                i = b'L0%dI%d&R1%dI%d\n' % (vertDist, vertDur, grindDist, grindDur)
+                OSRgrindLeft()
+                OSRgrindRight()
+                #i = b'L0%dI%d&R1%dI%d\n' % (vertDist, vertDur, grindDist, grindDur)
             elif ranRotate == 2:
                 #make sure grind stroke alternates above / below .5 
                 if last_grindDist >=500:
@@ -35,21 +84,14 @@ def OSR2():
             print(i)
             ser.write(i)
             #wait for motion to finish before sending next command
-            delay = vertDur/1000
-            last_vertDist = vertDist
+            delay = float(vertDur)/1000
+            last_vertDist = float(vertDist)/1000
 
             time.sleep(delay)
             
-def OSR0():
-    with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
-        while True:
-            i = b'L0999I3000\n'
-            time.sleep(3)
-            ser.write(i)
-            i = b'L0000I3000\n'
-            time.sleep(3)
-            ser.write(i)
-            print(i)
+
+
+
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
@@ -57,8 +99,10 @@ if __name__ == '__main__':
             OSR2()
         elif sys.argv[1] == str(3):
             OSR3()
+        elif sys.argv[1] == str('grind'):
+            OSRgrind()
         elif sys.argv[1] == str(0):
-            OSR0()
+            OSRneutral()
         else:
             OSR = input("2 for OSR2, 3 for OSR3")
             if OSR == 3:
