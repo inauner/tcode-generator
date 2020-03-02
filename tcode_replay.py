d7cfc1c3b2917b31d5b9ede895f7b1ff3e27a187
inauner <inaun@pm.me>
2020-03-01 22:02:19 -0800

    loop the pattern

--- tcode_replay.py
+++ tcode_replay.py
@@ -7,20 +7,24 @@ COM="COM4"
 
 def tcode_replay(filename):
     with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
-        f = open(filename,"r")
-        
-        for line in f.readlines():
-            line = line.rstrip()
-            tuples = re.findall(r'([Ii])(\d+)', line)
-            findmax = []
-            for tuple in tuples:
-                findmax.append(int(tuple[1]))
-            interval = max(findmax)
-            line = line + "\n"
-            line = line.encode()
-            print(line)
-            ser.write(line)
-            time.sleep(float(interval)/1000)
+        i = 1
+        while True:
+            print(i)
+            i+=1
+            f = open(filename,"r")
+            
+            for line in f.readlines():
+                line = line.rstrip()
+                tuples = re.findall(r'([Ii])(\d+)', line)
+                findmax = []
+                for tuple in tuples:
+                    findmax.append(int(tuple[1]))
+                interval = max(findmax)
+                line = line + "\n"
+                line = line.encode()
+                print(line)
+                ser.write(line)
+                time.sleep(float(interval)/1000)
         
         
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,42 @@
+import json
+import sys
+import os
+
+
+def Pattern1():
+
+def Pattern2():
+
+
+time.sleep(.02)
+
+
+def convert(input,output):
+    data = json.load(open(input, 'r'))
+    actions = (data)['actions']
+    # f = open(output,'w')
+    last_at=0
+    print(data['actions'][510])
+    print(data['rawActions'][510])
+
+    # for action in actions:
+        # pos = float(action['pos'])/100
+        # if pos == 1.0:
+            # pos = 0.999
+        # pos = pos * 1000
+        # at = action['at']
+        # i = "L0%dI%d" %(pos,at-last_at)
+        # print(i)
+        # f.write(i+'\n')
+        # last_at = at
+        
+    
+if __name__ == '__main__':
+    if len(sys.argv)==3:
+        convert(sys.argv[1],sys.argv[2])
+    elif len(sys.argv)==2:
+        output = os.path.splitext(sys.argv[1])[0]+".tcode"
+        convert(sys.argv[1],output)
+    else: 
+        print("drag funscript onto this script")
+        
