2a044dbd1e3fc70e7917ba5bb17c6b84a91a1482
inauner <inaun@pm.me>
2020-03-07 14:42:42 -0800

    gracefully exit

--- tcode_generator.py
+++ tcode_generator.py
@@ -2,123 +2,60 @@ import serial #pip install pySerial
 import time
 import random
 import sys
-import platform
-
-
+    
 #arduino COM port
 COM = 'COM4'
-
-# A function that tries to list serial ports on most common platforms
-def list_serial_ports():
-    system_name = platform.system()
-    if system_name == "Windows":
-        # Scan for available ports.
-        available = []
-        for i in range(256):
-            try:
-                s = serial.Serial(i)
-                available.append(i)
-                s.close()
-            except serial.SerialException:
-                pass
-        return available
-    elif system_name == "Darwin":
-        # Mac
-        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
-    else:
-        # Assume Linux or something else
-        return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
-        
-
-def OSRneutral():
-    with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
-        i = b'L05R15\n'
-        ser.write(i)
-
-def OSRgrind():
-    # with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
-    for n in range(5):
-        i = b'R1999I500\n'
-        time.sleep(.5)
-        ser.write(i)
-        i = b'R1000I500\n'
-        time.sleep(.5)
-        ser.write(i)
-        return(i)
-
+#stroke range. must be between 1 and 9999, but there's a float bug with values under 1000
+min_range=1000
+max_range=9999
+#time between strokes. must be greater than or equal to 0
+min_time=400
+max_time=1100
+    
+    
 def OSR2():
-    min_range=100
-    max_range=699
-    min_time=400
-    max_time=1100
+    #no need to change below variables. used for position state
     last_vertDist=0
     last_grindDist=500
     with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
         while True:
-            vertDist = float(random.randint(min_range,max_range))/1000 #need to make it a float for min. vert stroke distance
-            vertDur = random.randint(min_time,max_time) #length of time in ms for a stroke to complete
-            grindDur = random.randint(min_time,max_time)
-            while grindDur > vertDur:
+            try:
+                vertDist = float(random.randint(min_range,max_range))/10000 #need to make it a float for min. vert stroke distance
+                vertDur = random.randint(min_time,max_time) #length of time in ms for a stroke to complete
                 grindDur = random.randint(min_time,max_time)
-            #make sure vertical stroke distance is at least 250 between commands
-            
-            while abs(vertDist-last_vertDist)<.4:
-                vertDist = float(random.randint(min_range,max_range))/1000
-                # print(vertDist,last_vertDist,abs(vertDist-last_vertDist))
-                
-            vertDist = vertDist * 1000
-            #randomize a grinding motion
-            ranRotate = random.randint(1,2)
-            if ranRotate == 1:
-                #make sure grind stroke alternates above / below .5 
-                if last_grindDist >=500:
-                    grindDist = random.randint(100,200)
-                else: 
-                    grindDist = random.randint(800,999)
-                last_grindDist = grindDist
-                i = b'L0%dI%d&R1%dI%d\n' % (vertDist, vertDur, grindDist, grindDur)
-            else:
-                i = b'L0%dI%d&R1500I%d\n' % (vertDist, vertDur, vertDur)
-            print(i)
-            ser.write(i)
-            #wait for motion to finish before sending next command
-            delay = float(vertDur)/1000
-            last_vertDist = float(vertDist)/1000
-
-            time.sleep(delay)
-            
-
-
+                while grindDur > vertDur:
+                    grindDur = random.randint(min_time,max_time)
+                #make sure vertical stroke distance is at least x distance between commands
+                while abs(vertDist-last_vertDist)<.4:
+                    # print(vertDist,last_vertDist)
+                    vertDist = float(random.randint(min_range,max_range))/10000
+                vertDist = vertDist * 10000
+                #randomize a grinding motion
+                ranRotate = random.randint(1,2)
+                if ranRotate == 1:
+                    #make sure grind stroke alternates above / below .5 
+                    if last_grindDist >=500:
+                        grindDist = random.randint(0,2000)
+                    else: 
+                        grindDist = random.randint(800,9999)
+                    last_grindDist = grindDist
+                    i = b'L0%dI%d&R1%dI%d\n' % (vertDist, vertDur, grindDist, grindDur)
+                else:
+                    i = b'L0%dI%d&R1500I%d\n' % (vertDist, vertDur, vertDur)
+                print(i)
+                ser.write(i)
+                #wait for motion to finish before sending next command
+                delay = float(vertDur)/1000
+                last_vertDist = float(vertDist)/10000
+                time.sleep(delay)
+            #send back to default position on exit
+            except KeyboardInterrupt:
+                i = b'L05R15\n'
+                ser.write(i)  
+                sys.exit(0)
 
 
 if __name__ == '__main__':
-    if len(sys.argv)>1:
-        if sys.argv[1] == str(2):
-            OSR2()
-        elif sys.argv[1] == str(3):
-            OSR3()
-        elif sys.argv[1] == str('grind'):
-            OSRgrind()
-        elif sys.argv[1] == str(0):
-            OSRneutral()
-        else:
-            OSR = input("2 for OSR2, 3 for OSR3")
-            if OSR == 3:
-                OSR3()
-            else:
-                OSR2()
-    else:
-        OSR = input("2 for OSR2, 3 for OSR3")
-        if OSR == 3:
-            OSR3()
-        else:
-            OSR2()
+    OSR2()
 
 
-
-'''
-NOTES
-L0999I300&R1500I300
-SLOW: I9000, S0
-FAST: I0, S9000
-'''
--- tcode_replay.py
+++ tcode_replay.py
@@ -9,22 +9,27 @@ def tcode_replay(filename):
     with serial.Serial(COM,115200,timeout=1,parity=serial.PARITY_EVEN,rtscts=1) as ser:
         i = 1
         while True:
-            print(i)
-            i+=1
-            f = open(filename,"r")
-            
-            for line in f.readlines():
-                line = line.rstrip()
-                tuples = re.findall(r'([Ii])(\d+)', line)
-                findmax = []
-                for tuple in tuples:
-                    findmax.append(int(tuple[1]))
-                interval = max(findmax)
-                line = line + "\n"
-                line = line.encode()
-                print(line)
-                ser.write(line)
-                time.sleep(float(interval)/1000)
+            try:
+                print(i)
+                i+=1
+                f = open(filename,"r")
+                
+                for line in f.readlines():
+                    line = line.rstrip()
+                    tuples = re.findall(r'([Ii])(\d+)', line)
+                    findmax = []
+                    for tuple in tuples:
+                        findmax.append(int(tuple[1]))
+                    interval = max(findmax)
+                    line = line + "\n"
+                    line = line.encode()
+                    print(line)
+                    ser.write(line)
+                    time.sleep(float(interval)/1000)
+            except KeyboardInterrupt:
+                i = b'L05R15\n'
+                ser.write(i)  
+                sys.exit(0)
         
         
 if __name__ == '__main__':
